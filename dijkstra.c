7de359c09ca5e8d90cf17154f618977250333077
LeeonardoVargas <leu1607@hotmail.com>
2017-10-02 14:04:03 -0300

    dijkstra funcionando

--- dijkstra.c
+++ dijkstra.c
@@ -10,41 +10,37 @@ int matrix[chinelo][chinelo] = {
                                 {7,0,8,9,7,0,0},
                                 {0,8,0,0,5,0,0},
                                 {5,9,0,0,15,6,0},
-                                {0,7,5,15,0,8,9},
+                                {0,7,5,15,0,8,7},
                                 {0,0,0,6,8,0,11},
-                                {0,0,0,0,9,11,0}};
+                                {0,0,0,0,7,11,0}};
 int result[chinelo][2];
 // Linha 1: vertice anterior.
 // Linha 2: total.
 
 void dijstraAdjacencia(int v) {
     int a = 0, aux, auxTotal;
-    // printf("Adjacencia: %d --> ", v+1);
     do{
         aux = 1234567;
         visitados[v] = 1;
         for(a = 0; a < chinelo; a++){ // fazendo o somatorio na tabela
-            auxTotal = result[a][1] + matrix[v][a] + result[v][1];
+            auxTotal = matrix[v][a] + result[v][1];
             if(matrix[v][a] != 0 && visitados[a] != 1 && (result[a][1] == 0 || auxTotal < result[a][1])){
-                printf("entrou!! valor ant: %d, novo: %d, no vertice: %d\n", result[a][1], auxTotal, v);
+                printf("entrou!! valor ant: %d do vertice -> %d\nnovo: %d do vertice -> %d\n", result[a][1], result[a][0], auxTotal, v);
                 result[a][0] = v;
                 result[a][1] = auxTotal;
             }
         }
-        minhaTable();
-        for(a = 0; a < chinelo; a++){ // buscando o vertice com o menor total e não visitado ainda
-            if(visitados[a] != 1){
+        tabelaDijs();
+        for(a = 0; a < chinelo; a++) // buscando o vertice com o menor total e não visitado ainda
+            if(visitados[a] != 1)
                 if(result[a][1] < aux && result[a][1] > 0){
                     v = a;
                     aux = result[a][1];
                 }
-            }
-        }
-        printf("proximo vertice: %d, valor: %d \n", v, result[v][1]);
         if(visitados[v] == 1) return;
     }while(true);
 }
-void minhaTable(){
+void tabelaDijs(){
     int g;
     printf("Vertices anteriores: ");
     for(g = 0; g < chinelo; g++)
