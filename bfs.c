dad4a07e5ec631795282ddb94a5a3fb3ab21f3e0
LeeonardoVargas <leu1607@hotmail.com>
2017-10-04 20:51:29 -0300

    Ajustes codigo

--- bfs.c
+++ bfs.c
@@ -10,10 +10,9 @@
 // INCLUDES:
 #include <stdio.h>
 #include <stdlib.h>
-#include "bfs_functions.h"
 
 // DEFINES:
-#define INFINITE 9999 // Definição de Infinito para uso como custo máximo. (MAIOR_CUSTO_POSSIVEL)
+#define INFINITE 123456789 // Definição de Infinito para uso como custo máximo. (MAIOR_CUSTO_POSSIVEL)
 #define VERTEX_NUM 21 // Definição do número de vértices do grafo que será processado pela BFS. (BFS_NUM_VERTICES)
 #define WHITE 1 // Definição da cor Branca. (BRANCO)
 #define GREY 2 // Definição da cor Cinza. (CINZA)
@@ -29,52 +28,47 @@ int status[VERTEX_NUM];
 int list[VERTEX_NUM];
 
 // REPRESENTAÇÃO DO GRAFO 01:
-int ADJ_Graph[21][21] = {
-//   A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  Z
-	{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // A
-	{1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // B
-	{0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // C
-	{0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // D
-	{0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // E
-	{0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // F
-	{0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // G
-	{0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // H
-	{0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // I
-	{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // J
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0}, // K
-	{0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0}, // L
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0}, // M
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0}, // N
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0}, // O
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0}, // P
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0}, // Q
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0}, // R
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1}, // S
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0}, // T
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1}, // Z
-};
+int ADJ_Graph[VERTEX_NUM][VERTEX_NUM] = {
+	//   A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  Z
+		{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // A
+		{1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // B
+		{0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // C
+		{0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // D
+		{0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // E
+		{0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // F
+		{0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // G
+		{0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // H
+		{0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // I
+		{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // J
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0}, // K
+		{0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0}, // L
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0}, // M
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0}, // N
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0}, // O
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0}, // P
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0}, // Q
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0}, // R
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1}, // S
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0}, // T
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1}, // Z
+	};
 
 // FUNÇÕES DE CONTROLE DO ALGORITMO BFS:
 void BFS_ListInsert(int vertex, int vertexNum){
 	if(end == vertexNum - 1){
 		printf("[E] List Overflow.\n"); // Não é possível inserir na fila um número maior de vértices que o número máximo de vértices do grafo.
-    }
-    else{
-		if(top == -1){
+    } else {
+		if(top == -1)
 			top = 0;
-        }
 		end = end + 1; // Incrementa o fim da fila, informando que um novo vértice foi inserido.
 		list[end] = vertex; // Insere o vértice atual no final da lista, para fins de procura.
 	}
 }
 
 int BFS_EmptyList(){
-	if(top == -1 || top > end){
+	if(top == -1 || top > end)
         return 1; // Lista está vazia.
-    }
-	else{
-        return 0; // Lista não está vazia.
-    }
+    return 0; // Lista não está vazia.
 }
 
 int BFS_DeleteFromList(){
@@ -89,8 +83,7 @@ int BFS_DeleteFromList(){
 }
 
 void BFS_Run(int vertexNum, int fromVertex){
-	int i;
-	int v = fromVertex;
+	int i, v = fromVertex;
 	distance[v] = 0; // A distância do primeiro vértice inicial sempre será zero.
 	BFS_ListInsert(v, vertexNum); // Insere o primeiro vértice encontrado na lista de busca.
 	status[v] = GREY; // Seu status recebe GREY, já que ele foi encontrado.
@@ -109,14 +102,11 @@ void BFS_Run(int vertexNum, int fromVertex){
 	printf("\n");
 }
 
-void BFS_Initialize(int vertexNum){
-	int v;
-	for(v = 0; v < vertexNum; v++)
+void BFS_Initialize(){
+	for(int v = 0; v < VERTEX_NUM; v++){
 		status[v] = WHITE; // Inicialmente, todos os vértices são marcados como WHITE.
-	for(v = 0; v < vertexNum; v++)
-		distance[v] = -1; // Todas as distâncias são zeradas. -1 significa que ainda não sofreu nenhuma alteração.
-	for(v = 0; v < vertexNum; v++)
-		previousVertex[v] = -1; // Todos os vértices recebem -1 como anterior, significando que a BFS ainda não foi rodada.
+		previousVertex[v] = distance[v] = -1; // Todas as distâncias e os vértices são zerados. -1 significa que ainda não sofreu nenhuma alteração.
+	}
 }
 
 void BFS_Backtracking(int vertex){
@@ -124,44 +114,38 @@ void BFS_Backtracking(int vertex){
     int i;
 	char auxList[size - 1]; // Lista auxiliar para navegação.
 	int previous = previousVertex[vertex]; // O vértice anterior do atual é armazenado na variável auxiliar.
-	printf("\n[B] Vertex: %c. Backtracking: %c ", numVertex[vertex], numVertex[vertex]);
+	printf("\n[B] Vértice: %c. Backtracking: %c ", numVertex[vertex], numVertex[vertex]);
 	for(i = 0; i < size; i++){
 		auxList[i] = numVertex[previous]; // A lista sempre receberá o vértice anterior do vértice anterior, possibilitando o backtracking.
 		previous = previousVertex[previous];
 	}
-	for(i = 0; i < size; i++){
+	for(i = 0; i < size; i++)
 		printf("<- %c ", auxList[i]); // Imprime os vértices na sequencia inversa.
-	}
 }
 
 void BFS_Process(int vertexNum, int fromVertex, int backtrackingIndex){
-	BFS_Initialize(vertexNum); // Inicializando todos os controles da BFS.
+	BFS_Initialize(); // Inicializando todos os controles da BFS.
 	BFS_Run(vertexNum, fromVertex); // Executando a BFS a partir do vértice inicial.
     // Printando a TABELA final:
     printf("\n[T] Vértice:         ");
-	for(int i = 0; i < vertexNum; i++){
+	for(int i = 0; i < vertexNum; i++)
 		printf(" %2c", numVertex[i]);
-	}
     printf("\n[T] Distância:       ");
-	for(int k = 0; k < vertexNum; k++){
+	for(int k = 0; k < vertexNum; k++)
 		printf(" %2d", distance[k]);
-	}
     printf("\n[T] Vértice Anterior:");
-	for(j = 0; j < vertexNum; j++){
+	for(int j = 0; j < vertexNum; j++)
 		printf(" %2c", numVertex[previousVertex[j]]);
-	}
 	printf("\n");
     // Printando o Backtracking da execução da BFS:
 	BFS_Backtracking(backtrackingIndex);
 }
 
 // FUNÇÃO MAIN:
-int main(void)
-{
-	int graphNumVertex = 21;
+int main(void){
 	printf("[S] Iniciando o Programa:\n");
 	printf("[1] Executando os Processos da BFS para o Grafo 1:\n");
-	BFS_Process(graphNumVertex, 0, 20);
+	BFS_Process(VERTEX_NUM, 0, 20);
 	printf("\n\n[E] Final da Execução do Programa.\n");
 	return 0;
 }
--- dijkstra.c
+++ dijkstra.c
@@ -3,8 +3,8 @@
 #include <stdbool.h>
 #define chinelo 7 //numero de vertices
 #define vInicial 0
+#define vBusca 6
 int visitados[chinelo];
-// matriz de Adjacencia
 int matrix[chinelo][chinelo] = {
                                 {0,7,0,5,0,0,0},
                                 {7,0,8,9,7,0,0},
@@ -13,19 +13,32 @@ int matrix[chinelo][chinelo] = {
                                 {0,7,5,15,0,8,7},
                                 {0,0,0,6,8,0,11},
                                 {0,0,0,0,7,11,0}};
+char vertices[chinelo] = {'a','b','c','d','e','f','g'};
 int result[chinelo][2];
 // Linha 1: vertice anterior.
 // Linha 2: total.
-
+void tabelaDijs(){
+    printf("Vertices            : ");
+    for(int g = 0; g < chinelo; g++)
+        printf("%3c  |", vertices[g]);
+    printf("\nVertices anteriores : ");
+    for(int g = 0; g < chinelo; g++)
+        printf("%3c  |", vertices[result[g][0]]);
+    printf("\nTotal               : ");
+    for(int g = 0; g < chinelo; g++)
+        printf("%3d  |", result[g][1]);
+    printf("\n\n -------------------- \n\n");
+    return;
+}
 void dijstraAdjacencia(int v) {
     int a = 0, aux, auxTotal;
     do{
+        printf("Vertice -> %c\n\n", vertices[v]);
         aux = 1234567;
         visitados[v] = 1;
         for(a = 0; a < chinelo; a++){ // fazendo o somatorio na tabela
             auxTotal = matrix[v][a] + result[v][1];
             if(matrix[v][a] != 0 && visitados[a] != 1 && (result[a][1] == 0 || auxTotal < result[a][1])){
-                printf("entrou!! valor ant: %d do vertice -> %d\nnovo: %d do vertice -> %d\n", result[a][1], result[a][0], auxTotal, v);
                 result[a][0] = v;
                 result[a][1] = auxTotal;
             }
@@ -40,21 +53,18 @@ void dijstraAdjacencia(int v) {
         if(visitados[v] == 1) return;
     }while(true);
 }
-void tabelaDijs(){
-    int g;
-    printf("Vertices anteriores: ");
-    for(g = 0; g < chinelo; g++)
-        printf("%d  |  ", result[g][0]);
-    printf("\nTotal              : ");
-    for(g = 0; g < chinelo; g++)
-        printf("%d  |  ", result[g][1]);
-    printf("\n\n -------------------- \n\n");
-    return;
-}
 int main(){
     int g;
     for(g = 0; g < chinelo; g++)
         result[g][1] = result[g][0] = visitados[g] = 0;
     dijstraAdjacencia(vInicial);
+    printf("Vertice a ser buscado: %c\nCusto: %d\nCaminho: ", vertices[vBusca], result[vBusca][1]);
+    int v = vBusca;
+    while(true){
+        printf("%c <-- ", vertices[v]);
+        v = result[v][0];
+        if(v == 0) break;
+    }
+    printf("%c \n", vertices[v]);
     return 0;
 }
