5ffb96a6deedde621c7f94ddb4221b4111e49433
João M. Campagnolo <jota.campagnolo@gmail.com>
2017-10-04 12:05:37 -0300

    BFS pronta.

---
+++ bfs.c
@@ -0,0 +1,58 @@
+/*
+    Universidade Federal da Fronteira Sul - UFFS
+    Campus Chapecó
+    Ciência da Computação - CC
+    GEN039 - Grafos (Matutino - 2017/2)
+    Docente: Doglas André Finco
+    Alunos: João Marcos Campagnolo e Leonardo Vargas
+*/
+
+// INCLUDES:
+#include <stdio.h>
+#include <stdlib.h>
+#include "bfs_functions.h"
+
+// VARIÁVEIS DE CONTROLE DO PROGRAMA:
+char numVertex[VERTEX_NUM] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','z'}; // n_vertices
+int end = -1; // fim
+int top = -1; // topo
+int distance[VERTEX_NUM]; // dist
+int previousVertex[VERTEX_NUM]; // ver_anterior
+int status[VERTEX_NUM]; // estado
+int list[VERTEX_NUM]; // lista
+
+// REPRESENTAÇÃO DO GRAFO 01:
+int ADJ_Graph[21][21] = { // grafo1_adj
+	{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+};
+
+// FUNÇÃO MAIN:
+int main(void)
+{
+	int graphNumVertex = 21;
+	printf("[S] Iniciando o Programa:\n");
+	printf("[1] Executando os Processos da BFS para o Grafo 1:\n");
+	BFS_Process(graphNumVertex, 0, 20);
+	printf("[E] Final da Execução do Programa.\n");
+	return 0;
+}
